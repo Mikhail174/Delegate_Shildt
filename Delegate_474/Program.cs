a8b510d624e41a5acc3d067df5d87e4bdb165685
Mikhail <popovms1207@gmail.com>
2018-10-01 00:42:06 +0300

    Добавьте файлы проекта.

---
+++ Delegate_474/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_474
+{
+    delegate string StrMod(string str);
+    class DelegateTest {
+        //Заменить пробелы дефисами
+         static string ReplaceSpaces (string s)
+        {
+            Console.WriteLine("Замена пробелов дефисами");
+            return s.Replace(' ', '-');
+        }
+        //удалить пробелы
+        static string RemoveSpaces (string s)
+        {
+            string temp = "";
+            int i;
+            Console.WriteLine("Удаление пробелов");
+            for (i = 0; i < s.Length; i++)
+                if (s[i] != ' ')
+                    temp += s[i];
+            return temp;
+        }
+        //обратить строку
+        static string Reverse (string s)
+        {
+            string temp = "";
+            int i;
+            Console.WriteLine("Обращение строки");
+            for (i = s.Length - 1; i >= 0; i--)
+                temp += s[i];
+            return temp;
+        }
+        static void Main(string[] args)
+        {
+            //сконструировать делегат
+            StrMod strOp = new StrMod(ReplaceSpaces);
+            string str;
+            //Вызвать методы с помощью делегата
+            str = strOp("Это простой тест");
+            Console.WriteLine("Результирующая строка: " + str);
+            Console.WriteLine();
+
+            strOp = new StrMod(RemoveSpaces);
+            str = strOp("Это простой тест");
+            Console.WriteLine("Результирующая строка: " + str);
+            Console.WriteLine();
+
+            strOp = new StrMod(Reverse);
+            str = strOp("Это простой тест");
+            Console.WriteLine("Результирующая строка: " + str);
+
+
+        }
+    }
+
+}
