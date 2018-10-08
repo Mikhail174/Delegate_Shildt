581dd617c595ef79c82ec0916cbedc4046d28674
Mikhail <popovms1207@gmail.com>
2018-10-08 16:04:17 +0300

    08102018 16:04

---
+++ Delegate_487_VarCapture/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_487_VarCapture
+{
+    delegate int CountIt(int end); //этот делегат возвращает значение типа int и принимает аргумент типа int
+    class VarCapture
+    {
+        CountIt Counter()
+        {
+            int sum=0;
+            //здесь подсчитанная сумма сохраняется в переменной sum
+
+            CountIt ctObj = delegate (int end)
+           {
+               for (int i = 0; i <= end; i++)
+               {
+                   Console.WriteLine(i);
+                   sum += i;
+               }
+               return sum;
+           };
+            return ctObj;
+        }
+
+        static void Main(string[] args)
+        {
+            //получить результат подсчёта
+
+            VarCapture varCapture = new VarCapture();
+            CountIt count = varCapture.Counter();
+            int result;
+            result = count(3);
+            Console.WriteLine("Сумма 3 равна " + result);
+            Console.WriteLine();
+            result = count(5);
+            Console.WriteLine("Сумма 5 равна " + result);
+        }
+    }
+}
