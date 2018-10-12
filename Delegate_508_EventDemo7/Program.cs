d5960b4d89aafd3ec0048c89af272b49d4c88010
Mikhail <popovms1207@gmail.com>
2018-10-12 18:20:51 +0300

    //

---
+++ Delegate_508_EventDemo7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//использовать встроенный делегат EventHandler
+
+namespace Delegate_508_EventDemo7
+{//объявить класс, содержащий событие
+    class MyEvent
+    {
+        public event EventHandler SomeEvent; //использовать делегат EventHandler
+        public void OnSomeEvent()
+        {
+            if (SomeEvent != null)
+                SomeEvent(this, EventArgs.Empty);
+        } 
+    }
+    class EventDemo7
+    {
+        static void Handler (object source, EventArgs arg)
+        {
+            Console.WriteLine("Произошло событие");
+            Console.WriteLine( "Источник: "+source);
+        }
+        static void Main(string[] args)
+        {
+            MyEvent evt = new MyEvent();
+            //Добавить обработчик Handler() в цепочку событий
+            evt.SomeEvent += Handler;
+            //Запустить событие
+            evt.OnSomeEvent();
+        }
+    }
+}
---
+++ Delegate_509_KeyEventDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//пример обработки событий,связанных с нажатием клавиш на клавиатуре
+namespace Delegate_509_KeyEventDemo
+{//создать класс, производный от класса EventArgs и хранящий символ нажатой клавиши
+    class KeyEventArgs : EventArgs
+    {
+        public char ch;
+    }
+    //объявить класс события,связанного с нажатием клавиш на клавиатуре
+    class KeyEvent
+    {
+        public event EventHandler<KeyEventArgs> KeyPress; //обобщенный делегат
+        //этот метод вызывается при нажатии клавиши
+        public void OnKeyPress (char key)
+        {
+            KeyEventArgs k = new KeyEventArgs();
+            if (KeyPress != null)
+            {
+                k.ch = key;
+                KeyPress(this, k);
+            }
+        }
+    }
+    //продемонстрировать обработку события типа KeyEvent
+    class KeyEventDemo
+    {
+        static void Main(string[] args)
+        {
+            KeyEvent kevt = new KeyEvent();
+            ConsoleKeyInfo key;
+            int count = 0;
+
+            // использовать лямбда-выражение для отображения факта нажатия клавиши
+            kevt.KeyPress += (sender, e) => Console.WriteLine("Получено сообщение о нажатии клавиши: " + e.ch);
+
+            //использовать лямбда-выражение для подсчёта нажатых клавиш
+            kevt.KeyPress += (sender, e) => count++; //count - внешнаяя переменная
+
+            Console.WriteLine("ВВедите несколько символов, по завершении введите точку");
+            do
+            {
+                key = Console.ReadKey();
+                kevt.OnKeyPress(key.KeyChar);
+            } while (key.KeyChar != '.');
+            Console.WriteLine("Было нажато " + count + " клавиш");
+        }
+    }
+}
